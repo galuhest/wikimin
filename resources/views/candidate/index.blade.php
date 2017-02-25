ec04ca5009d2ee8929e4dccb802624e980f309ff
galuhestya <galuhestya@gmail.com>
2017-02-25 18:01:01 +0700

    controllers:

--- resources/views/candidate/index.blade.php
+++ resources/views/candidate/index.blade.php
@@ -10,8 +10,67 @@
     <a class="btn btn-default" href='{{url("/candidate/$candidate->id/edit")}}'>Edit</a>
     <!-- <img src='{{asset("app/photo/$candidate->name_head")}}'>
     <img src='{{asset("app/photo/$candidate->name_vice")}}'> -->
-    <p>Nama Calon Gubernur : {{\App\Person::where('id','=',$candidate->head_id)->first()->name}}</p>
-    <p>Nama Calon Wakil : {{\App\Person::where('id','=',$candidate->vice_id)->first()->name}}</p>
-
+    <div class="col-md-6">
+      <p>{{\App\Person::where('id','=',$candidate->head_id)->first()->name}}</p>
+    </div>
+    <div class="col-md-6">
+      <p>Nama Calon Wakil : {{\App\Person::where('id','=',$candidate->vice_id)->first()->name}}</p>
+    </div>
+    <div class="row">
+      <h3>Visi</h3>
+      <a class="btn" href="{{url('/visi/$candidate->id/create')}}">Tambah</a>
+      <a class="btn" href="{{url('visi/$candidate->id/edit')}}">Edit</a>
+      <a class="btn" href="{{url('visi/$candidate->id/')}}">Lihat</a>
+    </div>
+    <div class="row">
+      <h3>Program</h3>
+      <a class="btn" href="{{url('program/$candidate->id/create')}}">Tambah</a>
+      <a class="btn" href="{{url('program/$candidate->id/edit')}}">Edit</a>
+      <a class="btn" href="{{url('program/$candidate->id/')}}">Lihat</a>
+    </div>
+    <div class="row">
+      <h3>Pendidikan</h3>
+      <div class="col-md-6">
+        <a class="btn" href="{{url('education/$candidate->head_id/create')}}">Tambah</a>
+        <a class="btn" href="{{url('education/$candidate->head_id/')}}">Lihat</a>
+      </div>
+      <div class="col-md-6">
+        <a class="btn" href="{{url('education/$candidate->vice_id/create')}}">Tambah</a>
+        <a class="btn" href="{{url('education/$candidate->vice_id/')}}">Lihat</a>
+      </div>
+    </div>
+    <div class="row">
+      <h3>Karir</h3>
+      <div class="col-md-6">
+        <a class="btn" href="{{url('career/$candidate->head_id/create')}}">Tambah</a>
+        <a class="btn" href="{{url('career/$candidate->head_id/')}}">Lihat</a>
+      </div>
+      <div class="col-md-6">
+        <a class="btn" href="{{url('career/$candidate->vice_id/create')}}">Tambah</a>
+        <a class="btn" href="{{url('career/$candidate->vice_id/')}}">Lihat</a>
+      </div>
+    </div>
+    <div class="row">
+      <h3>Testimoni</h3>
+      <div class="col-md-6">
+        <a class="btn" href="{{url('testimony/$candidate->head_id/create')}}">Tambah</a>
+        <a class="btn" href="{{url('testimony/$candidate->head_id/')}}">Lihat</a>
+      </div>
+      <div class="col-md-6">
+        <a class="btn" href="{{url('testomony/$candidate->vice_id/create')}}">Tambah</a>
+        <a class="btn" href="{{url('testimony/$candidate->vice_id/')}}">Lihat</a>
+      </div>
+    </div>
+    <div class="row">
+      <h3>Kontroversi</h3>
+      <div class="col-md-6">
+        <a class="btn" href="{{url('controversy/$candidate->head_id/create')}}">Tambah</a>
+        <a class="btn" href="{{url('controversy/$candidate->head_id/')}}">Lihat</a>
+      </div>
+      <div class="col-md-6">
+        <a class="btn" href="{{url('controversy/$candidate->vice_id/create')}}">Tambah</a>
+        <a class="btn" href="{{url('controversy/$candidate->vice_id/')}}">Lihat</a>
+      </div>
+    </div>
   </div>
 @endforeach
