c35e0648144a55ac817fc861dfa794def3cfaaf6
galuhestya <galuhestya@gmail.com>
2017-03-03 07:45:51 +0700

    tambah dikit

--- resources/views/mission/create.blade.php
+++ resources/views/mission/create.blade.php
@@ -1,11 +1,17 @@
 @extends('template.app')
 @section('content')
 <form action="{{action('MissionController@store')}}" method="POST" class="form-horizontal">
-  <div class="col-md-4">
-    <input hidden type="text" value="{{$id}}" name="id">
-    <div class="form-group">
-      <label>Visi kandidat : </label><input class="form-control" type="text" name="misi[]" placeholder="misi">
-      <label> Sumber : </label><input class="form-control" type="text" name="source[]" placeholder="sumber">
+  <div class="col-md-12">
+    <div class="col-md-4">
+      <input hidden type="text" value="{{$id}}" name="id">
+      <div class="form-group">
+        <div class="form-group">
+          <label>Visi kandidat : </label><input class="form-control" type="text" name="misi[]" placeholder="misi">
+        </div>
+        <div class="form-group">
+          <label> Sumber : </label><input class="form-control" type="text" name="source[]" placeholder="sumber">
+        </div>
+      </div>
     </div>
   </div>
   {{csrf_field()}}
--- resources/views/vision/create.blade.php
+++ resources/views/vision/create.blade.php
@@ -1,11 +1,13 @@
 @extends('template.app')
 @section('content')
 <form action="{{action('VisionController@store')}}" method="POST" class="form-horizontal">
-  <div class="col-md-4">
-    <input hidden type="text" value="{{$id}}" name="id">
-    <div class="form-group">
-      <label>Visi kandidat : </label><input class="form-control" type="text" name="visi[]" placeholder="visi">
-      <label> Sumber : </label><input class="form-control" type="text" name="source[]" placeholder="sumber">
+  <div class="col-md-12">
+    <div class="col-md-4">
+      <input hidden type="text" value="{{$id}}" name="id">
+      <div class="form-group">
+        <label>Visi kandidat : </label><input class="form-control" type="text" name="visi[]" placeholder="visi">
+        <label> Sumber : </label><input class="form-control" type="text" name="source[]" placeholder="sumber">
+      </div>
     </div>
   </div>
   {{csrf_field()}}
